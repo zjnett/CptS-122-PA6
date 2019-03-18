fdfc55f265f032cd5d61e196fa8b54b7a8af7eaf
Zach Nett <zjnett@mac.com>
2019-03-18 09:50:54 -0700

    Added code for accessors/getters for BSTNode.

--- BSTNode.cpp
+++ BSTNode.cpp
@@ -5,3 +5,37 @@ Programming Assignment: PA6
 File: BSTNode.cpp
 Description: This program uses binary search trees to look up morse code.
 */
+
+#include "BSTNode.h"
+
+char BSTNode::getCharacter(void) {
+    return character;
+}
+
+string BSTNode::getMorse(void) {
+    return morse;
+}
+
+BSTNode *BSTNode::getLeft(void) {
+    return left;
+}
+
+BSTNode *BSTNode::getRight(void) {
+    return right;
+}
+
+void BSTNode::setCharacter(char newCharacter) {
+    character = newCharacter;
+}
+
+void BSTNode::setMorse(string newMorse) {
+    morse = newMorse;
+}
+
+void BSTNode::setLeft(BSTNode *newLeft) {
+    left = newLeft;
+}
+
+void BSTNode::setRight(BSTNode *newRight) {
+    right = newRight;
+}
